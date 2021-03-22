403ea24535c939d4afecc8e96242b7f64673e9e2
luanmirand <luan.miranda@academico.ifpb.edu.br>
2021-03-22 19:31:43 -0300

    Reajuste de Controller para Rescontroller

--- main/src/main/java/com/atividade/main/controller/AutorController.java
+++ main/src/main/java/com/atividade/main/controller/AutorController.java
@@ -1,10 +1,23 @@
 package com.atividade.main.controller;
 
+import java.net.URI;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.hibernate.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.atividade.main.model.Autor;
 import com.atividade.main.service.AutorService;
@@ -16,27 +29,37 @@ public class AutorController {
 
 	@Autowired
 	private AutorService autorService;
-
-	public void save(Autor autor) {
-		autorService.save(autor);
+	
+    @PostMapping("/save")
+    @ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Autor> save(@RequestBody Autor autor, HttpServletResponse response) {
+		Autor autorSave = autorService.save(autor);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+				.buildAndExpand(autor.getAutorId()).toUri();
+				response.setHeader("Location", uri.toASCIIString());
+		return ResponseEntity.created(uri).body(autorSave);		
 	}
-
-	public void edit(Autor autor) {
+    @PostMapping("/update")
+    @ResponseStatus(HttpStatus.CREATED)
+	public void edit(@RequestBody Autor autor) {
 		autorService.edit(autor);
 	}
-
+    
+    @PostMapping("/del")
 	public void excluir(long id) {
 		autorService.excluir(id);
 	}
 
-	public Autor findById(long id) {
-		return autorService.findById(id);
+	 @GetMapping("/{codigo}")
+	public ResponseEntity<Autor> findById(@PathVariable long id) {
+		 Autor autor = autorService.findById(id);
+		return autor!=null ? ResponseEntity.ok(autor) : ResponseEntity.notFound().build();
 	}
-
+	 @GetMapping("/autornome")  
 	public Autor getAutorPorNome(String nome) {
 		return autorService.getAutorPorNome(nome);
 	}
-
+	 @GetMapping("/lista")
 	public Page<Autor> getListaOrdenadaAsedente(Pageable page) {
 		return autorService.getListaOrdenadaAsedente(page);
 	}
